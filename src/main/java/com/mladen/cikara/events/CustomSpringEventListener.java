5c34ea7ea636760e62b5bc6c8871ba8c41aaea96
Mladen Čikara <mladen.cikara@gmail.com>
2018-07-24 15:03:59 +0200

    Explicit listener ordering

--- src/main/java/com/mladen/cikara/events/CustomSpringEventListener.java
+++ src/main/java/com/mladen/cikara/events/CustomSpringEventListener.java
@@ -3,21 +3,23 @@ package com.mladen.cikara.events;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomSpringEventListener implements ApplicationListener<CustomSpringEvent> {
+public class CustomSpringEventListener implements ApplicationListener<CustomSpringEvent>, Ordered {
   private static final Logger LOGGER = LoggerFactory.getLogger(CustomSpringEventListener.class);
+
+  @Override
+  public int getOrder() {
+    return Ordered.LOWEST_PRECEDENCE;
+  }
 
   @Override
   public void onApplicationEvent(CustomSpringEvent event) {
     LOGGER.info("Started receiving event");
 
-    try {
-      Thread.sleep(2000l);
-    } catch (final InterruptedException e) {
-      e.printStackTrace();
-    }
+    // throw new RuntimeException();
 
     LOGGER.info("Received spring custom event - {}", event.getMessage());
   }
--- src/main/java/com/mladen/cikara/events/SecondCustomSpringEventListener.java
+++ src/main/java/com/mladen/cikara/events/SecondCustomSpringEventListener.java
@@ -3,15 +3,26 @@ package com.mladen.cikara.events;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SecondCustomSpringEventListener implements ApplicationListener<CustomSpringEvent> {
+public class SecondCustomSpringEventListener
+    implements ApplicationListener<CustomSpringEvent>, Ordered {
   private static final Logger LOGGER =
       LoggerFactory.getLogger(SecondCustomSpringEventListener.class);
 
   @Override
+  public int getOrder() {
+    return Ordered.HIGHEST_PRECEDENCE;
+  }
+
+  @Override
   public void onApplicationEvent(CustomSpringEvent event) {
+    LOGGER.info("Started receiving event");
+
+    // throw new RuntimeException();
+
     LOGGER.info("Also received spring custom event - {}", event.getMessage());
   }
 
